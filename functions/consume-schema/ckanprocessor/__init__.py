e7bc8e46eaf767db0b43c63e52f7d2a9d7c68f3e
SpringPie <bente.vandepol@gmail.com>
2020-09-04 08:55:17 +0200

    Added debug info DAT-4031

--- functions/consume-schema/ckanprocessor/__init__.py
+++ functions/consume-schema/ckanprocessor/__init__.py
@@ -38,6 +38,8 @@ class CKANProcessor(object):
                     # If the urn of the processed schema coming from the topic
                     # is the same as one of the resources
                     if schema_urn_resource == urn_schema:
+                        logging.info("This resource has the same schema urn as the id of the schema message:")
+                        logging.info(resource)
                         # Give that resource a schema
                         self.patch_resource(resource, schema)
         else:
